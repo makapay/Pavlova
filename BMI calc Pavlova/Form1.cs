8ece68e963e39e24bfd23e49b0a60321820ae96f
makapay <118896931+makapay@users.noreply.github.com>
2023-03-30 11:23:17 +0300

    Add files via upload

---
+++ BMI calc Pavlova/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_calc_Pavlova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double rost = Convert.ToDouble(textBox1.Text) / 100;
+            double ves = Convert.ToDouble(textBox2.Text);
+            int BMI = Convert.ToInt32( ves / (rost * rost));
+            table10.text = BMI.ToString();
+            if (BMI < 10)
+            { trackBar1.Value = Convert.ToInt32(BMI); lable3.text =  }
+        }
+
+    }
+}
